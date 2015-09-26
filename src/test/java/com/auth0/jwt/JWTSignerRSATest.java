2f9d86037ab5844288ede3b9333be8beece61f31
Matthew Kastin <matthew.kastin@i-behavior.com>
2015-09-25 23:04:35 -0400

    passing first test

--- src/test/java/com/auth0/jwt/JWTSignerRSATest.java
+++ src/test/java/com/auth0/jwt/JWTSignerRSATest.java
@@ -3,48 +3,33 @@ package com.auth0.jwt;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.math.BigInteger;
 import java.security.*;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.spec.RSAPrivateKeySpec;
 import java.util.HashMap;
-import org.bouncycastle.util.encoders.Base64;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class JWTSignerRSATest {
-    private static PrivateKey privateKey;
-    private static PublicKey publicKey;
     private static JWTSigner signer;
 
     @BeforeClass
     public static void onlyOnce() throws Exception {
-        final KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
-        final SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
-        final byte[] seed = JWTSignerRSATest.class.getName().getBytes("UTF-8");
-        random.setSeed(seed);
-        keyGen.initialize(1024,random);
-        final KeyPair pair = keyGen.generateKeyPair();
-        privateKey = pair.getPrivate();
-        signer = new JWTSigner(privateKey);
-    }
+        BigInteger modulus = new BigInteger("AB34D0D48B16438BBBDFF6DA0CC7D3936DC2CE71E89DEF5B4AA9EA2539EAC17B5765FAAF2C533D176AF95CF16F157FECB977F51DE6E5473808E95A487321A3AB", 16);
+        BigInteger privateExponent = new BigInteger("0FEA1CEF64EE70E0F059E54C679BBBA31CB4DB13E397AAC445B07DBF701ECE554DE0266E99B96CE8F3148291551E70357E5AF29FB192FB9E5C2F4AA5C45A0141", 16);
 
-    public boolean verify(PublicKey publicKey,String signedData,String signature) {
-        Signature sig;
-        try {
-            sig = Signature.getInstance("SHA256withRSA");
-            sig.initVerify(publicKey);
-            sig.update(signedData.getBytes());
-            if (!sig.verify(Base64.decode(signature))) {
-                return false;
-            }
-            return true;
-        } catch ( Exception e ) {}
-        return false;
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        RSAPrivateKeySpec rsaPrivateKeySpec = new RSAPrivateKeySpec(modulus,privateExponent);
+        RSAPrivateKey rsaPrivateKey = (RSAPrivateKey) keyFactory.generatePrivate(rsaPrivateKeySpec);
+
+        signer = new JWTSigner(rsaPrivateKey);
     }
 
     @Test
     public void shouldSignEmpty() throws Exception {
         HashMap<String, Object> claims = new HashMap<String, Object>();
-        String token = signer.sign(claims,new JWTSigner.Options().setAlgorithm(Algorithm.RS256));
-        assertNotNull(token);
+        String token = signer.sign(claims, new JWTSigner.Options().setAlgorithm(Algorithm.RS256));
+        assertEquals("eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.e30.qL1rltWgwbxh1DXOGfehekZXPqZIgVaeOMc2Rd5wpHCH4ewlMCyw0fN32nj230RNDwB8Hj8ns6Nhwc6V8qWHpQ",token);
     }
 }
